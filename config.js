04d80e8373cc81b9e14481b724cd7013f92e9bc9
Dawid Brzozowski <d.brzozowski@wp.pl>
2018-03-11 20:26:07 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '641180463835-2a4dsabqn6h8uoo4kdhiqppl33n9b1aq.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': '0IiMQJg3oHnuhnjBsnBK9TCX',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
